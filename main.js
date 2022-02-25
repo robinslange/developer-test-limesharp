7e378953ecb0407f449892b13b2fb3b3b9efeb8e
Robin Lange <robinsimonlange@gmail.com>
2022-02-25 16:05:03 +1300

    tasks complete

---
+++ main.js
@@ -0,0 +1,41 @@
+repeat([1, 2, 3]);
+reformat("liMeSHArp DeveLoper TEST");
+next_binary_number([1,0,0,0,0,0,0,1]);
+
+function repeat(array) {
+  if (array.length <= 1) return;
+
+  let newArray = Array(3).fill(array).join(",").split(",");
+
+  console.log(newArray);
+}
+
+function reformat(string) {
+  if (string.length <= 0) return;
+
+  let vowels = /[aeiou]/gi;
+
+  let newString = string.replace(vowels, "");
+  newString =
+    newString.charAt(0).toUpperCase() + newString.slice(1).toLowerCase();
+
+  console.log(newString);
+}
+
+function next_binary_number(array) {
+  let newArray = array;
+  let flag = 0;
+  for (let i = newArray.length - 1; i >= 0; i--) {
+    if (newArray[i] == 0) {
+      newArray[i] = 1;
+      flag = 1;
+      break;
+    } else {
+      newArray[i] = 0;
+    }
+  }
+  if (flag < 0) {
+    newArray.unshift(1);
+  }
+  console.log(newArray);
+}
